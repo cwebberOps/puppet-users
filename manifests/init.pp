cab1484bf996d6cdeb0821f0ea93724e5008f28b
Christopher Webber <cwebber@ucr.edu>
2012-11-22 14:27:16 -0800

    Add initial functionality

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,23 @@
 # == Class: users
 #
-# Full description of class users here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+# This class is used to wrap user resources defined in hiera
 #
 # === Examples
 #
-#  class { users:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
+#  class { users: }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Christopher Webber <cwebberops@gmail.com>
 #
 # === Copyright
 #
-# Copyright 2011 Your name here, unless otherwise noted.
+# Copyright 2012 Christopher Webber, unless otherwise noted.
 #
 class users {
 
+  $users = hiera('users')
+
+  create_resources(user, $users)
 
 }
